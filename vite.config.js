b0a26815d624fd2d76d37c819ab990271fb6c7e6
Tom Carpenter <59624298+Luiginotcool@users.noreply.github.com>
2026-02-24 19:33:55 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [],
-  base: "/React_Vite_CICD_Github/",
+  base: "/",
   server: {
     open: true,
     port: 3001,
