a625caac61849c123dce14092723edaec98f1211
Tom Carpenter <59624298+Luiginotcool@users.noreply.github.com>
2026-02-24 19:39:42 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [],
-  base: "/",
+  base: "https://render.tomcarpenter.dev/",
   server: {
     open: true,
     port: 3001,
